810efffb7632d5ecd4fe1346172cf528bff2d5a1
Bojan Komazec <bojan.komazec@gmail.com>
2022-03-28 21:45:51 +0100

    tf file for creating a local file moved into separate directory. Added .gitignore in order to control which files created by TF should not be added to repository.

--- local_resources_demo/create_local_file_demo.tf
+++ local_resources_demo/create_local_file_demo.tf
@@ -7,7 +7,18 @@
 #
 # https://registry.terraform.io/providers/hashicorp/local/latest/docs/resources/file
 # https://www.terraform.io/language/expressions/references#filesystem-and-workspace-info
+#
+# Simple Terraform workflow:
+# $ terraform init - initializes the working directory containing this .tf file
+# $ terraform plan - to see and to review the execution plan
+# $ terraform apply - to apply changes
+# $ terraform show - to see the changes made
+# $ terraform destroy - to destroy created resources
+#
 resource "local_file" "foo" {
     filename = "${path.cwd}/temp/foo.txt"
     content = "This is a text content of the foo file!"
+
+    # Uncomment and execute 'terraform apply' again in order to change resources already deployed
+    # file_permission = "0700"
 }
